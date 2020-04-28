8c6cf0bdd5975bb960f2dd58d1bdf43ccbf568b4
Tuan Tran <dinh.anh.tuan.tran@nav.no>
2020-04-28 14:57:06 +0200

    added deployments dashboard

---
+++ scripts/pam-badges-in-one.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+# This script generates the file /docs/index.md
+
+names=()
+
+while IFS= read -r line; do
+    names+=( "$line" )
+done < <(find . -name "deploy-dev.yml" | sort | cut -d '/' -f 2)
+
+echo "# PAM deployments" > index.md
+echo "|    |    |    |    |" >> index.md
+echo "|:---|:---|:---|:---|" >> index.md
+count=0
+
+for name in "${names[@]}"
+do
+   count=`expr $count + 1`
+   remainder=`expr $count % 4`
+   printf "| $name ![build-deploy-dev](https://github.com/navikt/$name/workflows/build-deploy-dev/badge.svg) ![deploy-prod](https://github.com/navikt/$name/workflows/deploy-prod/badge.svg) " >> index.md
+   if [ "$remainder" == "0" ]; then
+     printf "|\n" >> index.md
+  fi
+done
