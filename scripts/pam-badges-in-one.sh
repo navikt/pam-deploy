eec90b1c3981fa190ca038af22236505bd2b8bd3
Tuan Tran <dinh.anh.tuan.tran@nav.no>
2020-04-29 08:00:06 +0200

    improve scripts

--- scripts/pam-badges-in-one.sh
+++ scripts/pam-badges-in-one.sh
@@ -1,25 +1,37 @@
 #!/usr/bin/env bash
 # This script generates the file /docs/index.md
 
+PAMFOLDER=$1
+if [ -z "$PAMFOLDER" ]; then
+  PAMFOLDER=$(pwd)
+fi
+
+echo "searching for pam projects in $PAMFOLDER"
+
 names=()
 
 while IFS= read -r line; do
     names+=( "$line" )
-done < <(find . -name "deploy-dev.yml" | sort | cut -d '/' -f 2)
+done < <(find $PAMFOLDER -name "deploy-dev.yml" | sort | rev | cut -d '/' -f 4 | rev )
 
-echo "# PAM deployments" > index.md
-echo "|    |    |    |    |" >> index.md
-echo "|:---|:---|:---|:---|" >> index.md
+FILE="index.md"
+
+echo "# PAM deployments" > $FILE
+echo "|    |    |    |    |" >> $FILE
+echo "|:---|:---|:---|:---|" >> $FILE
 count=0
 
 for name in "${names[@]}"
 do
-   if [ "$name" != "pam-scripts" ]; then
+   if [[ "$name" == pam-* ]]; then
+      echo "found project $name"
       count=`expr $count + 1`
       remainder=`expr $count % 4`
-      printf "| [$name](https://github.com/navikt/$name/actions) [![build-deploy-dev](https://github.com/navikt/$name/workflows/build-deploy-dev/badge.svg)](https://github.com/navikt/$name/releases) [![deploy-prod](https://github.com/navikt/$name/workflows/deploy-prod/badge.svg)](https://github.com/navikt/$name/releases/latest) " >> index.md
+      printf "| [$name](https://github.com/navikt/$name/actions) [![build-deploy-dev](https://github.com/navikt/$name/workflows/build-deploy-dev/badge.svg)](https://github.com/navikt/$name/releases) [![deploy-prod](https://github.com/navikt/$name/workflows/deploy-prod/badge.svg)](https://github.com/navikt/$name/releases/latest) " >> $FILE
       if [ "$remainder" == "0" ]; then
-        printf "|\n" >> index.md
+        printf "|\n" >> $FILE
       fi
    fi
 done
+
+echo "generated $FILE, move it to pam-deploy/docs/"
