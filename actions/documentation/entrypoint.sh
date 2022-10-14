714970ef9ca2611b1d1e352e9b045ece15e2d473
edvardgb <edvard.bakken@nav.no>
2022-10-14 14:22:15 +0200

    more logging

--- actions/documentation/entrypoint.sh
+++ actions/documentation/entrypoint.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 set -e
-
+echo "Hello before parser"
 /go/naisparser
 CONTENT=$(<tmp.json)
+echo "Hello after parser"
 
 echo "NAIS_CONTNENT=$CONTENT" >> "$GITHUB_ENV"
 echo $GITHUB_ENV
