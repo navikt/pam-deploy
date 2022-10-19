b975edcbae477b965f7540dce688aab303562110
edvardgb <edvard.bakken@nav.no>
2022-10-19 12:44:39 +0200

    tester gh permissions

--- actions/documentation/entrypoint.sh
+++ actions/documentation/entrypoint.sh
@@ -9,9 +9,9 @@ CONTENT=$(<tmp.json)
 
 #Set content as github environment
 # echo "NAIS_CONTENT=$CONTENT" >> "$GITHUB_ENV"
-
+curl  \
 REPOS=$(curl \
   -H "Accept: application/vnd.github+json" \
   -H "Authorization: Bearer {$GITHUB_TOKEN}" \
-  https://api.github.com/repos/navikt/repo-info/contents/repos.json)
+  https://api.github.com/repos/navikt/pam-repoinfo/contents/repos.json)
 echo $REPOS
