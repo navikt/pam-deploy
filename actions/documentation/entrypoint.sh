951a612123712e47dbb6fc48363e80a1585f3fbc
edvardgb <edvard.bakken@nav.no>
2022-10-25 14:26:12 +0200

    fix dubious ownership

--- actions/documentation/entrypoint.sh
+++ actions/documentation/entrypoint.sh
@@ -1,8 +1,14 @@
 #!/usr/bin/env bash
 set -e
 
+if [ -n "$GITHUB_WORKSPACE" ]; then
+  git config --global --add safe.directory $GITHUB_WORKSPACE
+  cd "$GITHUB_WORKSPACE" || exit
+fi
+
 # run go naisparser module
 /go/naisparser
+
 
 #Get content from tmpl file
 CONTENT=$(<tmp.json)
@@ -13,9 +19,6 @@ echo $CONTENT >> .doc/app.json
 
 rm tmp.json
 
-if [ -n "$GITHUB_WORKSPACE" ]; then
-  git config --global --add safe.directory $GITHUB_WORKSPACE
-  cd "$GITHUB_WORKSPACE" || exit
-fi
+
 
 git add .doc/app.json
