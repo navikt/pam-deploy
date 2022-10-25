c4cf1575f7b52029ee08a7a17a9a2c9506a091ca
edvardgb <edvard.bakken@nav.no>
2022-10-25 14:07:00 +0200

    git commit and push to .doc folder

--- actions/documentation/entrypoint.sh
+++ actions/documentation/entrypoint.sh
@@ -6,12 +6,9 @@ set -e
 
 #Get content from tmpl file
 CONTENT=$(<tmp.json)
+if [ ! -d ".doc" ]; then
+  mkdir -p ".doc"
+fi
+echo $CONTENT >> .doc/app.json
+rm tmp.json
 
-#Set content as github environment
-# echo "NAIS_CONTENT=$CONTENT" >> "$GITHUB_ENV"
-curl  \
-REPOS=$(curl \
-  -H "Accept: application/vnd.github+json" \
-  -H "Authorization: Bearer {$GITHUB_TOKEN}" \
-  https://api.github.com/repos/navikt/pam-repoinfo/contents/repos.json)
-echo $REPOS
