a30bae7295ca1b044b45c5554b9f3da1c60b5c3f
edvardgb <edvard.bakken@nav.no>
2022-10-26 12:23:23 +0200

    extract env as git_env

--- actions/documentation/entrypoint.sh
+++ actions/documentation/entrypoint.sh
@@ -1,34 +1,15 @@
 #!/usr/bin/env bash
 set -e
 
-if [ -n "$GITHUB_WORKSPACE" ]; then
-  git config --global --add safe.directory $GITHUB_WORKSPACE
-  cd "$GITHUB_WORKSPACE" || exit
-fi
-
 # run go naisparser module
 /go/naisparser
 
-
 #Get content from tmpl file
 CONTENT=$(<tmp.json)
-if [ ! -d ".doc" ]; then
-  mkdir -p ".doc"
-fi
-echo $CONTENT >> .doc/app.json
+echo $CONTENT
 
+#Get content as base64
 BASE_CONTENT=$(echo $CONTENT | base64)
+echo $BASE_CONTENT
 
-# git add .doc/app.json
-# curl \
-#   -X PUT \
-#   -H "Accept: application/vnd.github+json" \
-#   -H "Authorization: Bearer ${GITHUB_TOKEN}" \
-#   https://api.github.com/repos/navikt/${GITHUB_REPOSITORY}/contents/.doc/app.json \
-#   -d "{'message':'update','committer':{'name': 'GitHub Action', 'email':'action@github.com'},'content': ${BASE_CONTENT}}"
-
-# rm tmp.json
-
-# git remote set-url origin "https://${GITHUB_ACTOR}:@github.com/${GITHUB_REPOSITORY}.git"
-
-# git push -f
+echo "BASE_CONTENT=$BASE_CONTENT" >> "$GITHUB_ENV"
